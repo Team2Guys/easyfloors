c92a04b8880eb70f78789439ec22edaa9c0e8649
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2025-02-14 15:37:09 +0500

    fixes: refactor code and little adjustment✨

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,49 +1,5 @@
-'use client';
-
+import { footerData } from 'data/data';
 import Image from 'next/image';
-import { FaFacebookF, FaInstagram, FaPinterestP } from 'react-icons/fa';
-
-const footerData = {
-    company: {
-        name: 'easy floors',
-        description:
-            'Founded with a passion for quality and design. Easyfloors is committed to craftsmanship, customer satisfaction and great value for money.',
-    },
-    links: [
-        {
-            title: 'SPC FLOORING',
-            items: ['Richmond SPC Eco', 'Richmond SPC Prime', 'Richmond SPC Herringbone', 'Polar SPC Herringbone', 'Polar SPC'],
-        },
-        {
-            title: 'LVT FLOORING',
-            items: ['Richmond LVT Comfort', 'Richmond LVT Luxury', 'Polar LVT'],
-        },
-        {
-            title: 'RICHMOND FLOORING',
-            items: ['Richmond SPC Eco', 'Richmond SPC Prime', 'Richmond SPC Herringbone', 'Richmond LVT Comfort', 'Richmond LVT Luxury'],
-        },
-        {
-            title: 'POLAR FLOORING',
-            items: ['Polar SPC', 'Polar SPC Herringbone', 'Polar LVT'],
-        },
-        {
-            title: 'ACCESSORIES',
-            items: ['SPC Skirting', 'Stair Nose', 'T Profile', 'Reducer', 'Quarter Round'],
-        },
-    ],
-    contact: {
-        address: 'Agsons, J1 Warehouses, Jebel Ali Industrial – Dubai',
-        phone: '+971 50 597 4385',
-        email: 'cs@easyfloors.ae',
-    },
-    social: [
-        { icon: FaFacebookF, link: '#' },
-        { icon: FaInstagram, link: '#' },
-        { icon: FaPinterestP, link: '#' },
-    ],
-    paymentMethods: ['visa', 'apple-pay', 'tabby', 'mastercard', 'g-pay', 'tamara'],
-};
-
 const Footer = () => {
     return (
         <footer className="bg-gray-100 text-gray-700 pt-10 pb-5">
@@ -67,10 +23,10 @@ const Footer = () => {
                     <p className="text-sm mt-2">{footerData.contact.address}</p>
                     <p className="text-sm mt-2">{footerData.contact.phone}</p>
                     <p className="text-sm mt-2">{footerData.contact.email}</p>
-                    <div className="grid grid-cols-3 gap-0 mt-4">
+                    <div className="grid grid-cols-6 md:grid-cols-3  gap-0 mt-4">
                         {footerData.paymentMethods.map((method, index) => (
                             <div key={index} className="flex justify-center ">
-                                <Image src={`/assets/icons/${method}.png`} alt={method} width={100} height={100} className="" />
+                                <Image src={`/assets/icons/${method}.png`} alt={method} width={500} height={500} className="h-auto w-20" />
                             </div>
                         ))}
                     </div>
@@ -80,7 +36,7 @@ const Footer = () => {
                 <div className="flex space-x-4 ">
                     {footerData.social.map((social, index) => (
                         <a key={index} href={social.link} className="text-gray-600 hover:text-gray-900 bg-white p-1 rounded-md">
-                            <social.icon size={20} />
+                            <social.icon />
                         </a>
                     ))}
                 </div>
