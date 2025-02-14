9a229537c19a738e8f5e243ecf1979ad1024a012
misbahkhalil07 <misbahperveenpaf@gmail.com>
2025-02-14 18:00:55 +0500

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Container from "components/common/container/Container";
+import Features from "components/Main/features/features";
+import HeroMain from "components/Main/herosection/hero";
 
 export default function Home() {
   return (
-   <Container className="mt-20">
+    <Container className="mt-20">
      <h1 className="text-primary">Welcome to Easyfloors</h1>
+    <HeroMain/>
+    <Features/>
     </Container>
+   
   );
 }
