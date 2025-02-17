daa5d94fa1e5cab3230ebf38d5e660d3627cd140
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2025-02-17 17:42:47 +0500

    new section created

--- src/components/SampleBanner/SampleBanner.tsx
+++ src/components/SampleBanner/SampleBanner.tsx
@@ -5,17 +5,17 @@ import Container from 'components/common/container/Container';
 
 const SampleBanner: React.FC<{ imageData: TImageBanner }> = ({ imageData }) => {
   return (
-   <Container>
-     <div className="image-container w-full h-full">
-      <Image
-        src={imageData.src}
-        width={1500}
-        height={500}
-        alt={imageData.alt}
-        className="w-full h-full"
-      />
-    </div>
-   </Container>
+    <Container>
+      <div className="image-container w-full h-full">
+        <Image
+          src={imageData.src}
+          width={1500}
+          height={500}
+          alt={imageData.alt}
+          className="w-full h-full"
+        />
+      </div>
+    </Container>
   );
 };
 
