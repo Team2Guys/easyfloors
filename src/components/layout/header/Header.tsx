6b6a13826a6e740ffa31a72415bf653e43b38cb1
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2025-02-19 12:06:53 +0500

    whatsapp icons fixes

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import TopNav from './top-nav'
 import Navbar from './navbar'
-import Link from 'next/link'
-import Image from 'next/image'
 
 const Header = () => {
   return (
     <div className='mb-[32px] sm:mb-[38px] lg:mb-[70px]'>
     <TopNav/>
     <Navbar/>
-    <Link className='fixed bottom-20 right-10 lg:right-20 z-40 hidden sm:block' href="/">
-    <Image width={100} height={100} className=' w-16 h-16 2xl:w-[70px] 2xl:h-[70px]' src="/assets/images/icon/whatsapp-icon.png" alt='whatsapp-icon'/>
-    </Link>
     </div>
   )
 }
