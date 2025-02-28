82a3c5fea536e2e1d2b6d814090bd9a4021e26cd
MFaad1 <faadsardar123@gmail.com>
2025-02-28 17:10:07 +0500

    fxes

--- src/app/dashboard/usernewsletter/page.tsx
+++ src/app/dashboard/usernewsletter/page.tsx
@@ -1,21 +1,24 @@
-import axios from 'axios';
+
 import NewsLetter from './NewsLetter';
 
-const productHandler = async () => {
-  try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/newsletters/get-all`,
-    );
-    const data = response.data;
-    return data;
-  } catch (err) {
-    console.log('Error Occurred', err);
-    return [];
-  }
-};
+// const productHandler = async () => {
+//   try {
+//     const response = await axios.get(
+//       `${process.env.NEXT_PUBLIC_BASE_URL}/api/newsletters/get-all`,
+//     );
+//     const data = response.data;
+//     return data;
+//   } catch (err) {
+//     console.log('Error Occurred', err);
+//     return [];
+//   }
+// };
 
 const UserNewsletter = async () => {
-  const newsLetters = await productHandler();
+  /* eslint-disable */
+  const newsLetters:any = []
+  
+  // await productHandler();
   return <NewsLetter newsLetters={newsLetters} />;
 };
 
