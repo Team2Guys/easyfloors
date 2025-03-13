beb6adc26616871a3a5c0db1a2ff0f8ddf7cf197
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-03-13 12:06:46 +0500

    thumbnail dynamic faq

--- src/components/product-detail/thumbnail.tsx
+++ src/components/product-detail/thumbnail.tsx
@@ -4,27 +4,22 @@ import React, { useState, useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { ThumbnailProps } from "types/product-detail";
+import { ExtendedThumbnailProps } from "types/product-detail";
 
-interface ExtendedThumbnailProps extends ThumbnailProps {
-  hideThumnailBottom?: boolean;
-  imageheight?: boolean; // New prop to control visibility
-}
-
-const Thumbnail: React.FC<ExtendedThumbnailProps> = ({ ThumnailImage, ThumnailBottom, hideThumnailBottom = false, imageheight }) => {
+const Thumbnail = ({ ThumnailImage, ThumnailBottom, hideThumnailBottom = false, imageheight }:ExtendedThumbnailProps) => {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
   const sliderRef1 = useRef<Slider | null>(null);
 
   const handleThumbnailClick = (index: number) => {
     setCurrentSlide(index);
     if (sliderRef1.current) {
-      sliderRef1.current.slickGoTo(index); // Change the main image
+      sliderRef1.current.slickGoTo(index); 
     }
   };
 
+
   return (
     <div className="slider-container flex gap-2 sm:gap-4 overflow-hidden">
-      {/* Thumbnail List */}
       <div className="w-2/12">
         <div className="flex flex-col gap-1 sm:gap-2">
           {ThumnailImage.map((product, index) => (
@@ -78,8 +73,8 @@ const Thumbnail: React.FC<ExtendedThumbnailProps> = ({ ThumnailImage, ThumnailBo
                 <Image
                   width={150}
                   height={150}
-                  src={array.image}
-                  alt={array.title}
+                  src={array.imageUrl}
+                  alt={array.altText}
                   className="w-full max-sm:h-[39px] object-contain"
                 />
                 <p className="font-semibold text-[8px] md:text-14 lg:text-16">
