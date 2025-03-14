59332d1cf7827a91ec869c6c235d5d350d09166c
MFaad1 <faadsardar123@gmail.com>
2025-03-14 12:39:16 +0500

    fixes

--- src/accessories/entities/accessory.entity.ts
+++ src/accessories/entities/accessory.entity.ts
@@ -1,7 +1,7 @@
 import { Int, Field, ObjectType, ID } from '@nestjs/graphql';
 import { GraphQLJSON } from 'graphql-type-json';
 import { Product } from '../../products/entities/product.entity';
-import { Category } from 'categories/entities/category.entity';
+import { Category } from '../../categories/entities/category.entity';
 
 
 
