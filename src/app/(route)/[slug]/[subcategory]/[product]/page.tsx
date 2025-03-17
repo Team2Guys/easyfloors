d53464adf646e35ec724865df5098a9d8a8566ca
MFaad1 <faadsardar123@gmail.com>
2025-03-17 11:27:12 +0500

    fixes

--- src/app/(route)/[slug]/[subcategory]/[product]/page.tsx
+++ src/app/(route)/[slug]/[subcategory]/[product]/page.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { fetchProducts } from "config/fetch";
 import ProductDetail from "./ProductDetail";
+import { IProduct } from "types/prod";
+import { notFound } from "next/navigation";
 
-const Product = async ({params}:{params:Promise<{slug:string, subcategory: string, product:string}>}) => {
-  const {slug , subcategory , product} = await params;
-   const [ ProductInfo ] = await Promise.all([ fetchProducts()]);
+const Product = async ({ params }: { params: Promise<{ slug: string, subcategory: string, product: string }> }) => {
+  const { slug, subcategory, product: paramsprod } = await params;
+  const [ProductInfo] = await Promise.all([fetchProducts()]);
+  const productData = ProductInfo.find((product: IProduct) => (product?.custom_url?.trim() == paramsprod?.trim() && product?.category?.RecallUrl?.trim() === slug) && product.subcategory?.custom_url?.trim() == subcategory);
+
+  if (!productData) return notFound()
   return (
-    <ProductDetail MainCategory={slug} subCategory={subcategory} ProductName={product} ProductInfo={ProductInfo} />
+    <ProductDetail MainCategory={slug} subCategory={subcategory} ProductName={paramsprod} ProductInfo={ProductInfo} productData={productData} />
   );
 };
 
