8b8a7030687a73619d07fb0a846bb2b9e786eb1f
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-03-19 10:00:35 +0500

    build

--- src/components/cat_subcat/AddSubcategory.tsx
+++ src/components/cat_subcat/AddSubcategory.tsx
@@ -33,7 +33,7 @@ const FormLayout = ({
     ? {
       name: editCategory.name,
       description: editCategory.description || '',
-      category: editCategory?.category || 0,
+      category: editCategory?.category.id || 0,
       Meta_Title: editCategory.Meta_Title || '',
       short_description: editCategory.short_description || '',
       Meta_Description: editCategory.Meta_Description || '',
@@ -42,9 +42,7 @@ const FormLayout = ({
       whatamIdetails: editCategory?.whatamIdetails || [],
       whatAmiTopHeading: editCategory?.whatAmiTopHeading || "",
       Heading: editCategory?.Heading || "",
-      recalledByCategories: Array.isArray(editCategory?.recalledByCategories)
-        ? editCategory.recalledByCategories
-        : [],
+      recalledByCategories:editCategory?.recalledByCategories.map((value:Category)=>value.id) || []
 
     }
     : undefined;
