2600fb3fa752289bd763ca1f2b155a56056fd191
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-03-24 15:27:00 +0500

    cart accessory fixed

--- src/components/cart/cart-page.tsx
+++ src/components/cart/cart-page.tsx
@@ -121,6 +121,8 @@ const matchingProduct = products.find(
 const CategoryData = matchingProduct?.category
     ? { name: matchingProduct.category.name ?? "Unknown", RecallUrl: matchingProduct.category.RecallUrl ?? "" }
     : { name: "Unknown", RecallUrl: "" };
+
+    console.log(cartItems,"cartItems")
   return (
     
     <Container className='font-inter mt-10  mb-4 sm:mb-10 relative max-sm:max-w-[100%]'>
@@ -155,8 +157,8 @@ const CategoryData = matchingProduct?.category
                     <div>
                       <p className='text-12 sm:text-16 2xl:text-24 font-medium'>{item.name}</p>
                       <p className='text-12 sm:text-14 2xl:text-17'>Price: AED <span>{item.price}</span>/m<sup>2</sup></p>
-                      <p className='text-12 sm:text-14 2xl:text-17'>Price Per Box: <span className='font-bold'>AED {item.pricePerBox.toFixed(2)}</span></p>
-                      <p className='text-12 sm:text-14 2xl:text-17'>No. Of Boxes: <span className='font-bold'>{item.requiredBoxes ?? 0}</span> ({(Number(item.boxCoverage) * Number(item.requiredBoxes ?? 0)).toFixed(2) } SQM)
+                      <p className='text-12 sm:text-14 2xl:text-17'>{item.category === "Accessory" ? "Price Per Piece:" : "Price Per Box:"} <span className='font-bold'>AED {item.pricePerBox.toFixed(2)}</span></p>
+                      <p className='text-12 sm:text-14 2xl:text-17'>{item.category === "Accessory" ? "No. Of Piece:" : "No. Of Boxes:"} <span className='font-bold'>{item.requiredBoxes ?? 0}</span> ({(Number(item.boxCoverage) * Number(item.requiredBoxes ?? 0)).toFixed(2) } SQM)
                       </p>
                       <div className='flex xl:hidden gap-5 mt-2 items-center'>
                       <div className="flex items-center justify-center border border-[#959595] px-1 py-1 w-fit text-16 text-purple ">
