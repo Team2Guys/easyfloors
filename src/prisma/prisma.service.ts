2c7d1e2a72b7bbbf87f504db431e25f81b592c8f
MFaad1 <faadsardar123@gmail.com>
2025-04-05 12:08:40 +0500

    fixes

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      console.log('✅ Connected to the database');
+    } catch (error) {
+      console.error('❌ Failed to connect to the database:', error);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
