9ab47c54df37e89d619a9705d2826db34ecc889e
misbahkhalil07 <misbahperveenpaf@gmail.com>
2025-04-09 05:57:38 -0700

    wishlist updated

--- src/app/freesample/page.tsx
+++ src/app/freesample/page.tsx
@@ -1,6 +1,9 @@
+"use client"
 import dynamic from "next/dynamic";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ICart } from "types/prod";
+import { fetchItems } from "utils/cartutils";
 
 // Dynamic Imports
 const ProductTable = dynamic(() => import("components/product-table"));
@@ -9,13 +12,18 @@ const Container = dynamic(() => import("components/common/container/Container"))
 const Breadcrumb = dynamic(() => import("components/Reusable/breadcrumb"));
 const Top = dynamic(() => import("components/top"));
 const FreeSample = () => {
+  const [items, setItems] = useState<ICart[]>([]);
+  
+    useEffect(() => {
+      fetchItems(false, setItems); // Assuming 'false' is the appropriate value for the first argument
+    }, []);
   return (
     <>
       <Breadcrumb title="freesample" />
       <Container>
       <Top heading="Free Samples" Icon={() => <Image src="/assets/images/Wishlist/sample.svg" alt="Sample" width={24} height={24} className="h-10 w-10 lg:h-14 lg:w-14" />} />
       <div className=" hidden md:block pb-6 xl:pt-6 xl:mb-10">
-      <ProductTable columns={["Product", "Price Per Piece", "Stock Status", "Action"]} isSamplePage/>
+      <ProductTable columns={["Product", "Price Per Piece", "Stock Status", "Action"]} items={items} setItems={setItems} isSamplePage/>
       </div>
       <div className="block md:hidden">
       <WishlistSmall />
--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,7 +1,12 @@
-
+"use client";
 import dynamic from 'next/dynamic';
-import React from 'react'
-import { CiHeart } from 'react-icons/ci'
+import React, { useEffect, useState } from 'react';
+import { CiHeart } from 'react-icons/ci';
+import { ICart } from 'types/prod';
+import { fetchItems } from 'utils/cartutils';
+import Link from 'next/link';
+import { FaArrowLeftLong } from 'react-icons/fa6';
+
 // Dynamic Imports
 const ProductTable = dynamic(() => import("components/product-table"));
 const WishlistSmall = dynamic(() => import("components/smallscreen"));
@@ -10,23 +15,66 @@ const Breadcrumb = dynamic(() => import("components/Reusable/breadcrumb"));
 const Top = dynamic(() => import("components/top"));
 
 const Wishlist = () => {
+  const [items, setItems] = useState<ICart[]>([]);
+
+  useEffect(() => {
+    fetchItems(false, setItems); 
+  }, []);
+
+  const products = items.filter((item) => item.category !== "Accessories");
+  const accessories = items.filter(
+    (item) => item.category === "Accessories" || item.category === "Accessory"
+  );
+
+  if (items.length === 0) {
+    return (
+      <Container>
+        <Breadcrumb title="Wishlist" />
+        <Top heading="Wishlist" Icon={CiHeart} />
+        <div className="text-center my-7">
+          <h1 className="text-center xl:text-[48px]">Your Shopping Cart</h1>
+          <p className="text-center text-[24px] pt-10">Wishlist is empty</p>
+          <Link
+            href="/"
+            className="text-center text-[18px] bg-primary p-2 flex w-fit mx-auto items-center text-white gap-2 mt-4"
+          >
+            <FaArrowLeftLong /> Go Back to Shop
+          </Link>
+        </div>
+      </Container>
+    );
+  }
+  
   return (
     <>
       <Breadcrumb title="Wishlist" />
       <Container>
         <Top heading="Wishlist" Icon={CiHeart} />
+        {products.length > 0 && (
         <div className="hidden md:block pb-6 xl:pt-6 xl:mb-10">
           <ProductTable
             columns={["Product", "Price Per Box", "QTY(m/m2)", "Stock Status", "Action"]}
+            items={products} 
+            setItems={setItems} 
           />
         </div>
-       {/* <AccessoryTable/> */}
+         )}
+
+        {accessories.length > 0 && (
+        <div className="hidden md:block pb-6 xl:pt-6 xl:mb-10">
+          <ProductTable
+            columns={["Accessories", "Price Per m", "QTY m", "Stock Status", "Action"]}
+            items={accessories} 
+            setItems={setItems} 
+          />
+        </div>
+        )}
         <div className="block md:hidden mb-5">
-          <WishlistSmall/>
+          <WishlistSmall />
         </div>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default Wishlist
+export default Wishlist;
