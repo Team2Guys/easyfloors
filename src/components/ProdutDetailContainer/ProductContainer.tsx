d56ce5c86eb270a56d866af436e6ff0aab7665a3
MFaad1 <faadsardar123@gmail.com>
2025-04-15 14:20:45 +0500

    made the Add to wishlist similar to add to cart

--- src/components/ProdutDetailContainer/ProductContainer.tsx
+++ src/components/ProdutDetailContainer/ProductContainer.tsx
@@ -152,7 +152,7 @@ const ProductContainer = ({ MainCategory, subCategory, productData, className, i
           </button>
         </div>
         <button
-          className="flex text-[#475156] gap-2 items-center"
+          className="flex bg-black justify-center text-11 xs:text-12 text-white w-5/12 2xl:text-22 font-inter gap-2 items-center max-sm:h-[40px] px-2 py-2 sm:py-3 sm:text-"
           onClick={() =>
             handleAddToStorage(
               productData,
