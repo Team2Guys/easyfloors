de0d3d1542dc612f3f39a566851c4090934fe40a
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-04-21 16:51:35 +0500

    ui fixes

--- src/app/shipping-policy/page.tsx
+++ src/app/shipping-policy/page.tsx
@@ -32,26 +32,26 @@ const Shipping = () => {
         for any clarification or further information regarding our delivery process. Your satisfaction is our priority.
       </p>
       
-      <h2 className=" text-20 sm:text-28 font-semibold font-inter">Express Shipping- Next-day delivery</h2>
+      <h2 className=" text-20 sm:text-24 font-semibold font-inter">Express Shipping- Next-day delivery</h2>
       <div className='space-y-2'>
-      <h2 className=" text-18 sm:text-24 font-semibold font-inter">Shipping Fee:</h2>
-      <ul className='list-disc px-6'>
-        <li>All Emirates- <span className="font-currency font-normal text-18"></span> 150</li>
-        <li>Free shipping for all orders above <span className="font-currency font-normal text-18"></span> 1000</li>
+      <h2 className="text-14 sm:text-20 sm:leading-[26px] font-inter font-semibold">Shipping Fee:</h2>
+      <ul className='list-disc px-6 text-14 sm:text-20 sm:leading-[26px] font-inter'>
+        <li>All Emirates- <span className="font-currency font-normal text-16 sm:text-25"></span> 150</li>
+        <li>Free shipping for all orders above <span className="font-currency font-normal text-16 sm:text-25"></span> 1000</li>
       </ul>
       <p className='text-14 sm:text-20 sm:leading-[26px] font-inter'>*Free Express delivery is available for orders above <span className="font-currency font-normal text-16 sm:text-25"></span> 1000. Orders less than <span className="font-currency font-normal text-16 sm:text-25"></span> 1000 will be charged according to the location.</p>
       </div>
 
-      <h2 className=" text-20 sm:text-28 font-semibold font-inter">Standard Shipping- Within 2-3 working days</h2>
+      <h2 className=" text-20 sm:text-24 font-semibold font-inter">Standard Shipping- Within 2-3 working days</h2>
       <div className='space-y-2'>
-      <h2 className=" text-18 sm:text-24 font-semibold font-inter">Shipping Fee: <span className='font-normal'>Free for all Emirates</span></h2>
+      <h2 className="text-14 sm:text-20 sm:leading-[26px] font-inter font-semibold">Shipping Fee: <span className='font-normal'>Free for all Emirates</span></h2>
       <p className='text-14 sm:text-20 sm:leading-[26px] font-inter'>We also have a self-collect option available where you can select the products on the website and collect them at our Jebel Ali Industrial warehouse.</p>
       </div>
       <div className='space-y-2'>
-      <h2 className="text-20 sm:text-28 font-semibold font-inter">Self Collect: Mon-Sat (9 am-6 pm)</h2>
-      <p className=" text-18 sm:text-24 font-semibold font-inter">Location: <Link href="https://maps.app.goo.gl/VoKEfBJLA2y9fySt5" className='font-normal underline hover:text-primary'>Agsons, J1 Warehouses, Jebel Ali Industrial – Dubai</Link></p>
+      <h2 className="text-20 sm:text-24 font-semibold font-inter">Self Collect: Mon-Sat (9 am-6 pm)</h2>
+      <p className="text-14 sm:text-20 sm:leading-[26px] font-inter">Location: <Link href="https://maps.app.goo.gl/VoKEfBJLA2y9fySt5" className='font-normal underline hover:text-primary'>Agsons, J1 Warehouses, Jebel Ali Industrial – Dubai</Link></p>
       </div>
-      <h2 className=" text-20 sm:text-28 font-semibold font-inter">CASH ON DELIVERY: NOT AVAILABLE</h2>
+      <h2 className=" text-20 sm:text-24 font-semibold font-inter">CASH ON DELIVERY: NOT AVAILABLE</h2>
     </Container>
   );
 };
