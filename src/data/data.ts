e7bf345de07ddb2cbc4bd619fc3bb593abdc855b
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-04-25 10:11:42 +0500

    optimze code

--- src/data/data.ts
+++ src/data/data.ts
@@ -267,8 +267,9 @@ export const FindUs = [
 
 export const heroItems: HeroItem[] = [
   {
-    // backgroundImage: "/assets/images/Home/hero-min.webp",
-    backgroundImage: "https://res.cloudinary.com/dmmeqgdhv/image/upload/v1744112788/hero-min_2_ixg8qo.webp",
+    backgroundImage: "/assets/images/Home/hero-min.avif",
+    backgroundImageMobile: "/assets/images/Home/hero-minmobile.webp",
+    // backgroundImage: "https://res.cloudinary.com/dmmeqgdhv/image/upload/v1744112788/hero-min_2_ixg8qo.webp",
     offerText: "Limited Time Offer",
     highlight: "",
     description:
