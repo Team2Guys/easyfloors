d108fcfdb287c87bd9037a34095b46e81df57be2
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-05-09 17:52:22 +0500

    sorting

--- src/data/accessory.ts
+++ src/data/accessory.ts
@@ -4,9 +4,9 @@
     "Stair Nose",
     "Quarter Round",
     "L Shape Skirting 10cm",
-    "L Shape Skirting 15cm",
     "L Shape Skirting 12cm",
+    "L Shape Skirting 15cm",
     "Skirting 8cm",
+    "Skirting 10cm",
     "Skirting 12cm",
-    "Skirting 15cm",
   ];
