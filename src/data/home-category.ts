2c8b9ae351705f5706f18cc5ad82413babb85038
Wajid-Fareed <wajidfareed103@gmail.com>
2025-06-03 18:33:38 +0500

    fixes

--- src/data/home-category.ts
+++ src/data/home-category.ts
@@ -5,13 +5,13 @@ export const getSubcategoryOrder = (name: string) => {
   if (lower.includes("polar spc")) return 1;
   if (lower.includes("polar lvt")) return 3;
 
-  if (lower.includes("richmond spc herringbone")) return 5;
+  if (lower.includes("richmond spc herringbone")) return 4;
   if (lower.includes("richmond spc prime")) return 4;
   if (lower.includes("richmond spc eco")) return 3;
   if (lower.includes("richmond spc")) return 4;
 
   // Specific Richmond LVT types
-  if (lower.includes("richmond lvt comfort")) return 4;
+  if (lower.includes("richmond lvt comfort")) return 5;
   if (lower.includes("richmond lvt luxury")) return 5;
   if (lower.includes("richmond lvt")) return 6;
 
