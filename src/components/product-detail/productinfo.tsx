31a247d21881d11454f1c0651b369e0ac303d88e
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-06-27 10:56:30 +0500

    accessory issue fixed

--- src/components/product-detail/productinfo.tsx
+++ src/components/product-detail/productinfo.tsx
@@ -73,38 +73,33 @@ const SkirtingProductDetail = ({ productData, MainCategory, image, selectedColor
         <div className="flex border-b-[1px] border-gray-300"></div>
       </div>
 
-      <div className="w-full mt-1 h-216 border border-black px-3 pb-1">
-        <p className="font-semibold font-inter text-16 xl:text-20 ">Colour: <span className="font-light text-14 xl:text-19">{selectedColor?.colorName}</span></p>
-        {uniqueFeatureImages.length > 0 ?
-          <div className="grid grid-cols-8 gap-2 lg:gap-2 mt-1">
-            {uniqueFeatureImages.map((col, index) => (
-              <div key={index} className={`text-center border cursor-pointer w-full lg:w-12 ${selectedColor?.color === col.color ? 'border-primary' : 'border-transparent'}`} onClick={() => handleColorClick(col)}>
-                <Image alt="img" src={col.imageUrl} height={1000} width={1000} className="h-auto w-full lg:h-12" />
-                <p className="text-[8px] sm:text-10 font-inter font-normal text-nowrap">{col.color}</p>
-              </div>
-            ))}
-          </div>
-          :
-          <p className="font-inter font-light text-14 xl:text-16">No colour found</p>
+        {uniqueFeatureImages && uniqueFeatureImages.length > 0 &&
+        <div className="w-full mt-1 h-216 border border-black px-3 pb-1">
+          <p className="font-semibold font-inter text-16 xl:text-20 ">Colour: <span className="font-light text-14 xl:text-19">{selectedColor?.colorName}</span></p>
+            <div className="grid grid-cols-8 gap-2 lg:gap-2 mt-1">
+              {uniqueFeatureImages.map((col, index) => (
+                <div key={index} className={`text-center border cursor-pointer w-full lg:w-12 ${selectedColor?.color === col.color ? 'border-primary' : 'border-transparent'}`} onClick={() => handleColorClick(col)}>
+                  <Image alt="img" src={col.imageUrl} height={1000} width={1000} className="h-auto w-full lg:h-12" />
+                  <p className="text-[8px] sm:text-10 font-inter font-normal text-nowrap">{col.color}</p>
+                </div>
+              ))}
+            </div>
+        </div>
         }
-
-      </div>
+        {matchingColor && matchingColor.length > 0 &&
       <div
         className={`mt-3 p-3 border border-black h-36 ${matchingColor.length > 3 ? 'overflow-auto' : 'overflow-hidden'
           }`}
       >
         <p className="font-semibold text-15 xl:text-[23.6px] font-inter">Matching with:</p>
-        {matchingColor.length ? (
-          matchingColor.map((item, index) => (
+        {matchingColor.map((item, index) => (
             <p className="font-inter font-light text-14 xl:text-16" key={index}>
               {item.name}
             </p>
           ))
-        ) : (
-          <p className="font-inter font-light text-12 xl:16">No colour match</p>
-        )}
+        }
       </div>
-
+        }
       {/* Length Input */}
       <div className="border border-black mt-2 p-3">
         <div className="flex items-center gap-2 ">
