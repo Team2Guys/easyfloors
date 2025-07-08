f4db0129d2a8523ecc1149cd3bb4e3c85ebbafcd
Wajid-Fareed <wajidfareed103@gmail.com>
2025-07-08 17:51:36 +0500

    fixes

--- src/components/Reusable/SampleBanner.tsx
+++ src/components/Reusable/SampleBanner.tsx
@@ -1,8 +1,8 @@
-
+'use client'
 import Container from 'components/common/container/Container'
 import Image from 'next/image'
 const bannerImage = '/assets/images/Home/free_sample_image.webp'
-import React from 'react'
+const truckImage = '/assets/images/Home/truckLogo.webp'
 
 const SampleBanner = () => {
   return (
@@ -17,24 +17,27 @@ const SampleBanner = () => {
           <Image
             src={bannerImage}
             alt='Free Sample'
-            priority
+            loading='lazy'
             fill
             quality={85}
             className='!relative'
           />
         </div>
-        <div className='flex border-b-[#00000033] border-b-[1px] justify-between w-full items-center mt-5 sm:border-b-2 relative'>
+        <div className='flex border-b-[#00000033] border-b-[1px] justify-between w-full items-end mt-5 sm:border-b-2 relative'>
           <div className="w-6/12 xs:w-7/12 lg:w-2/3 overflow-hidden md:w-8/12 2xl:w-9/12">
-            <Image src='https://res.cloudinary.com/dmmeqgdhv/image/upload/v1742291653/delivery-truck_11zon_hsjurq.webp'
-              height={800}
-              width={800}
-              alt='image'
-              priority
-              className='h-5 w-10 animate-moveTruck sm:h-[64px] sm:w-24 xl:h-[122px] xl:w-[238px]'
-            />
+            <div className='h-5 w-10 sm:h-[64px] sm:w-24 xl:h-[122px] xl:w-[238px]'>
+              <Image
+                // src='https://res.cloudinary.com/dmmeqgdhv/image/upload/v1742291653/delivery-truck_11zon_hsjurq.webp'
+                src={truckImage}
+                fill
+                alt='image'
+                loading='lazy'
+                className='animate-moveTruck !relative'
+              />
+            </div>
           </div>
-          <div className='h-auto absolute bg-white right-0 bottom-[0.5px]'>
-            <h2 className='text-14 text-end font-inter 2xl:text-4xl font-medium lg:text-2xl md:pr-7 md:text-lg pr-2 sm:text-base xl:text-3xl'>Free Samples UAE Wide</h2>
+          <div className='bg-white'>
+            <h2 className='text-14 text-end text-nowrap font-inter 2xl:text-4xl font-medium lg:text-2xl md:pr-7 md:text-lg pr-2 sm:text-base xl:text-3xl'>Free Samples UAE Wide</h2>
           </div>
         </div>
       </div>
